81576511560573ece5172188d664b8ab8c38f513
hyeokkiyaa <khm38607574@gmail.com>
2024-08-10 23:31:23 +0900

    Main page added to move next page

--- app/src/main/java/com/example/dogroulettegame/MainActivity.java
+++ app/src/main/java/com/example/dogroulettegame/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.dogroulettegame;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,9 +21,19 @@ public class MainActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            ImageButton ib = (ImageButton) findViewById(R.id.imageButton);
+            ib.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view)
+                {
+                    Intent intent = new Intent(getApplicationContext(), Main_game.class);
+                    startActivity(intent);
+                }
+            });
             return insets;
         });
         startService(new Intent(getApplicationContext(), MusicService.class));
+
     }
 
     @Override
---
+++ app/src/main/java/com/example/dogroulettegame/Main_game.java
@@ -0,0 +1,14 @@
+package com.example.dogroulettegame;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main_game extends AppCompatActivity {
+    @Override
+    protected void  onCreate(@Nullable Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game_main);
+    }
+}
