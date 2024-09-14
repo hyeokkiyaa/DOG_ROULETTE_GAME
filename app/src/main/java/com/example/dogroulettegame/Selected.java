056916d7a7d655b2cb6d88c62f699056111611e1
hyeokkiyaa <khm38607574@gmail.com>
2024-09-14 22:36:05 +0900

    slight changes made for data movement

--- app/src/main/java/com/example/dogroulettegame/Selected.java
+++ app/src/main/java/com/example/dogroulettegame/Selected.java
@@ -9,6 +9,7 @@ public class Selected extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.selected);
     }
 }
